63198b8b0f65ac5bd522f1d69e5a1245b64851c8
Yuvan Sankar Raj <u1sankarraj1@gmail.com>
2026-04-15 11:19:20 +0530

    Initial commit

---
+++ src/main/java/WebhookDummy/DummyVer/Main.java
@@ -0,0 +1,10 @@
+package WebhookDummy.DummyVer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*******************************Hey this is the Dummy Version********************************");
+	}
+
+}
