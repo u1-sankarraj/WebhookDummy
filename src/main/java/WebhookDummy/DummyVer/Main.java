2e30cd6799998c4be633090b26489b0a23b623c8
Yuvan Sankar Raj <u1sankarraj1@gmail.com>
2026-04-15 11:42:14 +0530

    This is the first initial push ver 1

--- src/main/java/WebhookDummy/DummyVer/Main.java
+++ src/main/java/WebhookDummy/DummyVer/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("*******************************Hey this is the Dummy Version********************************");
+		System.out.println("*******************************This is the firt Pushhh********************************");
 	}
 
 }
